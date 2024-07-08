72d54872930f862e0f0d018a8aee0d6adbc49a4c
Jody Clark <jodyclark09@gmail.com>
2024-07-08 19:31:03 +0000

    Completed typescript-primitives-and-variables exercise.

---
+++ typescript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+'use strict';
+const fullName = 'Jody Clark';
+const isCool = false;
+const totalPets = 1;
+console.log('value of fullName: ', fullName);
+console.log('value of isCool: ', isCool);
+console.log('value of totalPets: ', totalPets);
+console.log('type of fullName: ', typeof fullName);
+console.log('type of isCool: ', typeof isCool);
+console.log('type of totalPets: ', typeof totalPets);
+const empty = null;
+console.log('value of null: ', empty);
+console.log('type of null: ', typeof empty);
+let nothing;
+console.log('value of nothing: ', nothing);
+console.log('type of nothing: ', typeof nothing);
---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,18 @@
+const fullName: string = 'Jody Clark';
+const isCool: boolean = false;
+const totalPets: number = 1;
+
+console.log('value of fullName: ', fullName);
+console.log('value of isCool: ', isCool);
+console.log('value of totalPets: ', totalPets);
+console.log('type of fullName: ', typeof fullName);
+console.log('type of isCool: ', typeof isCool);
+console.log('type of totalPets: ', typeof totalPets);
+
+const empty: null = null;
+console.log('value of null: ', empty);
+console.log('type of null: ', typeof empty);
+
+let nothing: undefined;
+console.log('value of nothing: ', nothing);
+console.log('type of nothing: ', typeof nothing);
