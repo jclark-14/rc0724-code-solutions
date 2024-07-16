a0055a65d07cbe25db9659bccdbedf2ffe75b2a6
Jody Clark <jodyclark09@gmail.com>
2024-07-16 14:57:18 +0000

    practice-exercises

--- practice-exercises/02-basic-challenges-2/05-are-all-chars-unique/are-all-chars-unique.ts
+++ practice-exercises/02-basic-challenges-2/05-are-all-chars-unique/are-all-chars-unique.ts
@@ -1,3 +1,8 @@
 export function areAllCharactersUnique(str: string): boolean {
-  return false;
+  for (let i = 0; i < str.length; i++) {
+    if (str.indexOf(str[i], i + 1) > 0) {
+      return false;
+    }
+  }
+  return true;
 }
