32e941fe7b8467823ee711fcb487c3e28237b8a8
Jody Clark <jodyclark09@gmail.com>
2024-07-16 01:34:08 +0000

    practice exercises

--- practice-exercises/02-basic-challenges-2/04-find-missing-letter/find-missing-letter.ts
+++ practice-exercises/02-basic-challenges-2/04-find-missing-letter/find-missing-letter.ts
@@ -1,3 +1,23 @@
 export function findMissingLetter(arr: string[]): string | undefined {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let formattedStr = '';
+  let letter = '';
+  for (let i = 0; i < arr.length; i++) {
+    const formattedChar = arr[i].toLowerCase();
+    formattedStr += formattedChar;
+  }
+
+  const strI = alphabet.indexOf(formattedStr[0]);
+  const alphabetSlice = alphabet.slice(strI, strI + arr.length);
+
+  for (let j = 0; j < formattedStr.length; j++) {
+    if (formattedStr[j] !== alphabetSlice[j]) {
+      letter = alphabetSlice[j];
+      if (alphabet.indexOf(arr[0]) < 0) {
+        letter = letter.toUpperCase();
+      }
+      return letter;
+    }
+  }
   return undefined;
 }
