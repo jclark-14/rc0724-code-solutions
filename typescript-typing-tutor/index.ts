8a5753e0b9d0a70d48ab6e1d1176e3afeb4435ec
Jody Clark <jodyclark09@gmail.com>
2024-07-23 22:07:07 +0000

    Completed typescript-typing-tutor challenge.

---
+++ typescript-typing-tutor/index.ts
@@ -0,0 +1,22 @@
+const $div = document.querySelector('div');
+const $spans = document.querySelectorAll('span');
+if (!$div || !$spans) throw new Error('The query failed');
+console.log($spans);
+let keyCount = 0;
+$div.addEventListener('keydown', (event: KeyboardEvent) => {
+  console.log(event.key);
+  console.log(event);
+  if ($spans[keyCount].innerText === event.key) {
+    keyCount++;
+    if (keyCount < $spans.length) {
+      $spans[keyCount].className = 'marker';
+    }
+    $spans[keyCount - 1].className = 'green';
+  } else if ($spans[keyCount].innerText === ' ' || event.code === 'Space') {
+    keyCount++;
+    $spans[keyCount].className = 'marker';
+    $spans[keyCount - 1].className = 'green';
+  } else {
+    $spans[keyCount].className = 'wrong-key';
+  }
+});
