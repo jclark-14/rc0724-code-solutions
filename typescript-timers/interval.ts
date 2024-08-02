1989f2a9f614fa84f9b6176529469eedfe132f47
Jody Clark <jodyclark09@gmail.com>
2024-08-02 17:58:00 +0000

    Completed typescript-timers exercise.

---
+++ typescript-timers/interval.ts
@@ -0,0 +1,13 @@
+const $h2 = document.querySelector('h1');
+if (!$h2) throw new Error('query failed');
+
+const a: string[] = ['3', '2', '1', '~Earth Beeeelooowww Us~'];
+let i = 0;
+const id = setInterval(() => {
+  $h2.textContent = a[i];
+  console.log(($h2.textContent = a[i]));
+  i++;
+  if (i === 4) {
+    clearInterval(id);
+  }
+}, 1000);
