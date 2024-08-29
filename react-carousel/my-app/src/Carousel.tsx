c0d02483c1c6396a05825028e27b99d6d1b64662
Jody Clark <jodyclark09@gmail.com>
2024-08-29 18:38:02 +0000

    Completed react-carousel challenge.

---
+++ react-carousel/my-app/src/Carousel.tsx
@@ -0,0 +1,77 @@
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+import { GoDotFill } from 'react-icons/go';
+import { useEffect, useState } from 'react';
+
+type Image = {
+  src: string;
+  alt: string;
+};
+type Props = {
+  images: Image[];
+};
+
+type ImageCardProps = {
+  image: Image;
+};
+
+type NavDotsProps = {
+  images: Image[];
+  onClick: (index: number) => void;
+};
+
+export function Carousel({ images }: Props) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  function handleNextClick() {
+    setCurrentIndex(currentIndex === images.length - 1 ? 0 : currentIndex + 1);
+  }
+
+  function handlePrevClick() {
+    setCurrentIndex(currentIndex === 0 ? images.length - 1 : currentIndex - 1);
+  }
+
+  function handleNavDotClick(index: number) {
+    setCurrentIndex(index);
+  }
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 1500);
+    return () => clearTimeout(timerId);
+  }, [images.length, currentIndex]);
+
+  return (
+    <div className="container">
+      <div className="img-nav-row">
+        <a href="#">
+          <FaAngleLeft size="90px" onClick={handlePrevClick} />
+        </a>
+        <ImageCard image={images[currentIndex]} />
+        <a href="#">
+          <FaAngleRight size="90px" onClick={handleNextClick} />
+        </a>
+      </div>
+      <NavDots images={images} onClick={handleNavDotClick} />
+    </div>
+  );
+}
+
+function ImageCard({ image }: ImageCardProps) {
+  return (
+    <div className="img-card">
+      <img src={image.src} alt={image.alt} className="img" />
+    </div>
+  );
+}
+
+function NavDots({ images, onClick }: NavDotsProps) {
+  const navDots = images.map((image, index) => (
+    <a href="#" key={index}>
+      <GoDotFill size="50px" onClick={() => onClick(index)} />
+    </a>
+  ));
+  return <div className="dots-div">{navDots}</div>;
+}
